2f5ba04acb9aa0c271f3a6d63ead76f8fd953edd
guest <guest@gmail.com>
2024-08-28 17:19:56 +0800

    remove extra files

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React from 'react';
-import Child1 from './Child1';
-import Child2 from './Child2';
-import Child3 from './Child3';
 import './App.css'; 
 import logo from './logo.svg';
-import Registration from './Registration';
 import LogAdmin from './Dashboard/LogAdmin';
 
 
 function App() {
     return (
-    
         <div>
             <LogAdmin/>
         </div>
