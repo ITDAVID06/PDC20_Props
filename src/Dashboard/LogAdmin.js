cccfde2cb4f747c4ff0a450fca49d5681eed7588
guest <guest@gmail.com>
2024-09-11 14:58:56 +0800

    login attempts and error message

--- src/Dashboard/LogAdmin.js
+++ src/Dashboard/LogAdmin.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import AdminPanel from './AdminPanel';
 import './LogAdmin.css'; 
+import { useNavigate } from 'react-router-dom';
 
 function LogAdmin() {
     const [username, setUsername] = useState('');
@@ -8,9 +9,32 @@ function LogAdmin() {
     const [accountType, setAccountType] = useState('');
     const [user, setUser] = useState(null);
 
+    const [loginAttempts, setLoginAttempts] = useState(0);
+    const [errorMessage, setErrorMessage] = useState('');
+    
+    const navigate = useNavigate();
+
     const handleLogin = () => {
+
+        const validAdminCredentials = {
+            username: 'user123',
+            password: 'pass123'
+        };
+
         const isAdmin = accountType === 'admin';
-        setUser({ name: username, isAdmin });
+        if(isAdmin && (username === validAdminCredentials.username && password === validAdminCredentials.password)){
+            setUser({ name: username, isAdmin });
+        } else if(!isAdmin && (username === validAdminCredentials.username && password === validAdminCredentials.password)) {
+            setUser({ name: username, isAdmin : false });
+        } else {
+            setLoginAttempts(prevAttempts => prevAttempts + 1);
+            setErrorMessage('Invalid Credentials');
+
+            if (loginAttempts +1 >= 3){
+                navigate('');
+            }
+        }
+        
     };
 
     return (
